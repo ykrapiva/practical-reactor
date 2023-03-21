1d670107decc2acf8ca2f05b06bcfa7e61019069
Ievgen Krapyva <ievgen.krapyva@gmail.com>
2023-03-21 10:59:05 +0200

    Practice

--- exercises/src/test/java/c3_FilteringSequence.java
+++ exercises/src/test/java/c3_FilteringSequence.java
@@ -85,8 +85,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
     @Test
     public void dont_take_more_then_you_need() {
         Flux<Integer> numbers = number_service()
-                //todo: change this line only
-                ;
+                .take(100);
 
         StepVerifier.create(numbers)
                     .expectNextCount(100)
