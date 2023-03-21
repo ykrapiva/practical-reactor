f7533e650dc57da74a9202fdd2ecbe9c406ef07a
Ievgen Krapyva <ievgen.krapyva@gmail.com>
2023-03-21 11:09:19 +0200

    Practice

--- exercises/src/test/java/c4_LifecycleHooks.java
+++ exercises/src/test/java/c4_LifecycleHooks.java
@@ -111,8 +111,7 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
         AtomicBoolean canceled = new AtomicBoolean(false);
 
         Flux<Integer> temperatureFlux = room_temperature_service()
-                //todo: change this line only
-                ;
+                .doOnCancel(() -> canceled.set(true));
 
         StepVerifier.create(temperatureFlux.take(0))
                 .expectNextCount(0)
@@ -131,8 +130,7 @@ public class c4_LifecycleHooks extends LifecycleHooksBase {
         AtomicInteger hooksTriggeredCounter = new AtomicInteger(0);
 
         Flux<Integer> temperatureFlux = room_temperature_service()
-                //todo: change this line only
-                ;
+                .doOnTerminate(hooksTriggeredCounter::incrementAndGet);
 
         StepVerifier.create(temperatureFlux.take(0))
                 .expectNextCount(0)
